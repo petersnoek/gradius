532c7b79d081a6840b245f93fc6d6b1e740422bd
Peter Snoek <petersnoek@davinci.nl>
2014-05-07 14:45:13 +0200

    no message

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
